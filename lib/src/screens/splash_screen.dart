230b5ddb23bbe34451fc66e7f4713b10e0888fd4
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-20 03:01:33 +0530

    Added credential check at splash_screen

--- lib/src/screens/splash_screen.dart
+++ lib/src/screens/splash_screen.dart
@@ -1,6 +1,37 @@
+import '../firebase/firebase.dart';
+import 'login_screen.dart';
+import 'home_screen.dart';
+import '../provider.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    checkForCredentials();
+  }
+
+  void checkForCredentials() async {
+    final response = await AuthApi.get();
+    if (!response.isSuccessful)
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => LoginScreen()));
+    else if (response.result.isEmailVerified) {
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => LoginScreen()));
+    } else {
+      AuthApi api = response.result;
+      Provider.of(context).state.init(api);
+      Navigator.of(context)
+          .push(MaterialPageRoute(builder: (context) => HomeScreen()));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold();
