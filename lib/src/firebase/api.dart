51fe3c2d78559fc65b694f9e66c86b3df1d0a605
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-20 00:01:25 +0530

    Added Api Interface

--- lib/src/firebase/api.dart
+++ lib/src/firebase/api.dart
@@ -10,4 +10,28 @@ mixin Api {
 
 // Default Implementation
 
-class FirebaseApi with Api {}
+class FirebaseApi with Api {
+  @override
+  Future<Response> addCollection(Collection collection) {
+    // TODO: implement addCollection
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> addNote(Note note) {
+    // TODO: implement addNote
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> updateCollection(Collection collection) {
+    // TODO: implement updateCollection
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Response> updateNote(Note note) {
+    // TODO: implement updateNote
+    throw UnimplementedError();
+  }
+}
