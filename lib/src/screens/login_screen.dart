5e9451fa9a481348cca7900947e6210dec45954c
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-20 23:34:26 +0530

    Added EmailId and Password field

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,8 +1,39 @@
+import '../utils/utils.dart';
+import '../views/views.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatelessWidget with ScreenUtilMixin {
+  final GlobalKey<FormFieldState> _formKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(sw(20)),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              AppTitle(),
+              vGap(32),
+              Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: 'EmailId'),
+                  ),
+                  vGap(20),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: 'Password'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
