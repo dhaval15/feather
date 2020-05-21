52f54f0153e709e40debc4ba54b05f3d60dbb9ae
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-21 21:59:32 +0530

    Added Action icons in HomeScreen

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../style/style.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -7,7 +8,25 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Feather'),
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(AppIcons.sort),
+            onPressed: _sortPopup,
+          ),
+          IconButton(
+            icon: Icon(AppIcons.view),
+            onPressed: _viewPopup,
+          ),
+        ],
       ),
     );
+  }
+
+  void _sortPopup() {
+    //TODO implement Sorting Facility : Time, ModifiedDate, Name, CreateDate.
+  }
+
+  void _viewPopup() {
+    //TODO implement View Changing Facility : List or Grid.
   }
 }
