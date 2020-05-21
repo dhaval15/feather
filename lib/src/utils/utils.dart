6d9ff52ece76533b45d8ea65756f579e26f0ec68
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-21 14:25:33 +0530

    Added email and password validators

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,2 +1,3 @@
 export 'screen_utils.dart';
 export 'model.dart';
+export 'validators.dart';
---
+++ lib/src/utils/validators.dart
@@ -0,0 +1,10 @@
+class Validators {
+  static bool emailId(String text) => RegExp(
+          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+      .hasMatch(text);
+
+  static bool password(String text) =>
+      text.contains(RegExp(r'[a-zA-Z]')) &&
+      text.contains(RegExp(r'[0-9]')) &&
+      text.length > 6;
+}
