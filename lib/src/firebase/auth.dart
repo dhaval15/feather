45a2961b8e79f0013371fd30efc7f67776b42d46
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-23 11:01:55 +0530

    Added several apis to facilate user acount mangement

--- lib/src/firebase/auth.dart
+++ lib/src/firebase/auth.dart
@@ -2,7 +2,52 @@ import 'package:feather/src/firebase/response.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthApi {
-  Future<Response> sendVerificationLink(String emailId) async {
+  final FirebaseUser user;
+
+  AuthApi(this.user);
+
+  static Future<Response<AuthApi>> get() async {
+    try {
+      FirebaseUser user = await FirebaseAuth.instance.currentUser();
+      if (user != null)
+        return Response.success(AuthApi(user));
+      else
+        throw Error();
+    } catch (e) {
+      return Response.failure(e);
+    }
+  }
+
+  Future<Response> logout() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      return Response.success(null);
+    } catch (e) {
+      return Response.failure(e);
+    }
+  }
+
+  Future<Response> reload() async {
+    try {
+      await user.reload();
+      return Response.success(null);
+    } catch (e) {
+      return Response.failure(e);
+    }
+  }
+
+  Future<Response> updateProfile(String displayName) async {
+    try {
+      await user.updateProfile(UserUpdateInfo()..displayName = displayName);
+      return Response.success(null);
+    } catch (e) {
+      return Response.failure(e);
+    }
+  }
+
+  String get displayName => user.displayName;
+
+  static Future<Response> sendVerificationLink(String emailId) async {
     try {
       await FirebaseAuth.instance.sendSignInWithEmailLink(
         email: emailId,
