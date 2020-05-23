771f928a47a0162ac497be663e104c1a7d5c1b2a
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-23 10:37:07 +0530

    Email Login rebased to passwordless login - using links

--- lib/src/firebase/auth.dart
+++ lib/src/firebase/auth.dart
@@ -2,78 +2,17 @@ import 'package:feather/src/firebase/response.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthApi {
-  final FirebaseUser user;
-
-  const AuthApi(this.user);
-
-  static Future<Response<AuthApi>> get() async {
+  Future<Response> sendVerificationLink(String emailId) async {
     try {
-      final user = await FirebaseAuth.instance.currentUser();
-      if (user != null)
-        return Response.success(AuthApi(user));
-      else
-        throw Error();
-    } catch (e) {
-      return Response.failure(e);
-    }
-  }
-
-  static Future<Response<AuthApi>> signInWithEmailAndPassword(
-      String emailId, String password) async {
-    try {
-      final authResult = await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: emailId, password: password);
-      return Response.success(AuthApi(authResult.user));
-    } catch (e) {
-      return Response.failure(e);
-    }
-  }
-
-  Future<Response> sendVerificationLink() async {
-    try {
-      await user.sendEmailVerification();
-      return Response.success(null);
-    } catch (e) {
-      return Response.failure(e);
-    }
-  }
-
-  bool get isEmailVerified => user.isEmailVerified;
-
-  static Future<Response<bool>> isAlreadyUser(String emailId) async {
-    try {
-      final authMethods = await FirebaseAuth.instance
-          .fetchSignInMethodsForEmail(email: emailId);
-      return Response.success(authMethods.isNotEmpty);
-    } catch (e) {
-      return Response.failure(e);
-    }
-  }
-
-  static Future<Response<AuthApi>> createUserWithEmailAndPassword(
-      String emailId, String password) async {
-    try {
-      final authResult = await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(email: emailId, password: password);
-      return Response.success(AuthApi(authResult.user));
-    } catch (e) {
-      return Response.failure(e);
-    }
-  }
-
-  static Future<Response> sendPasswordResetEmail(String emailId) async {
-    try {
-      await FirebaseAuth.instance.sendPasswordResetEmail(email: emailId);
-      return Response.success(null);
-    } catch (e) {
-      return Response.failure(e);
-    }
-  }
-
-  static Future<Response> confirmPassword(
-      String oobCode, String emailId) async {
-    try {
-      await FirebaseAuth.instance.confirmPasswordReset(oobCode, emailId);
+      await FirebaseAuth.instance.sendSignInWithEmailLink(
+        email: emailId,
+        url: 'https://feather.brokenglass.com/login',
+        handleCodeInApp: true,
+        iOSBundleID: '',
+        androidMinimumVersion: '12',
+        androidPackageName: 'com.brokenglass.feather',
+        androidInstallIfNotAvailable: true,
+      );
       return Response.success(null);
     } catch (e) {
       return Response.failure(e);
