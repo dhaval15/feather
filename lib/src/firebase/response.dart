f208bda63c4c72696288e3a2490e88ecab75d3d3
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-23 14:56:18 +0530

    Bug fixed

--- lib/src/firebase/response.dart
+++ lib/src/firebase/response.dart
@@ -1,10 +1,18 @@
 class Response<T> {
   final T result;
-  final Error error;
+  final Exception exception;
   final bool isSuccessful;
 
-  Response._({this.result, this.error}) : isSuccessful = result != null;
+  Response._({this.result, this.exception}) : isSuccessful = exception == null;
 
   factory Response.success(T result) => Response._(result: result);
-  factory Response.failure(Error error) => Response._(error: error);
+  factory Response.failure(Exception exception) =>
+      Response._(exception: exception);
+
+  @override
+  String toString() => {
+        'isSuccessful': isSuccessful,
+        'result': result,
+        'exception': exception,
+      }.toString();
 }
