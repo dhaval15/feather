3ff7ba0f842dc5b763eb5885be2469c4ca4720ca
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-26 21:02:49 +0530

    Added Platform side api for fetching link from intent

--- android/app/src/main/kotlin/com/brokenglass/feather/MainActivity.kt
+++ android/app/src/main/kotlin/com/brokenglass/feather/MainActivity.kt
@@ -1,6 +1,21 @@
 package com.brokenglass.feather
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val methodChannel = MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger,"plugins.broken-glass.com/feather")
+        print("Activity : --------------------------- ${intent.data}" )
+        methodChannel.invokeMethod("onIntentData",intent.data?.toString())
+    }
 }
