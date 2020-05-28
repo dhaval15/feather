72f8806163d378e37676d75082172d756863d469
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-28 22:11:31 +0530

    Updated Icons

--- lib/src/style/icons.dart
+++ lib/src/style/icons.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class AppIcons {
-  //TODO : Assign and add icons data.
-  static IconData note, sort, collection, idea, view;
+  static const IconData notes = FlutterIcons.note_oct;
+  static const IconData sort = FlutterIcons.sort_mdi;
+  static const IconData collections = FlutterIcons.folder_mdi;
+  static const IconData ideas = FlutterIcons.thought_bubble_mco;
+  static const IconData account = FlutterIcons.account_mco;
+  static const IconData view = FlutterIcons.view_grid_mco;
+  static const IconData add_note = FlutterIcons.feather_fea;
+  static const IconData right = FlutterIcons.done_mdi;
 }
