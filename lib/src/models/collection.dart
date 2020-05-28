9d9059ca6e3ca539dded5d0c21d6f8af4fc86575
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-28 22:15:12 +0530

    Added Synicing Capabilities

--- lib/src/models/collection.dart
+++ lib/src/models/collection.dart
@@ -2,25 +2,35 @@ class Collection {
   final String key;
   final String title;
   final String description;
+  final bool synced;
 
-  Collection({this.key, this.title, this.description});
+  Collection({
+    this.key,
+    this.title,
+    this.description,
+    this.synced = false,
+  });
 
   factory Collection.fromJson(dynamic json) => Collection(
         key: json['k'],
         title: json['t'],
         description: json['d'],
+        synced: json['s'],
       );
 
-  Collection copyWith({String key, String title, String description}) =>
+  Collection copyWith(
+          {String key, String title, String description, bool synced}) =>
       Collection(
         key: key ?? this.key,
         title: title ?? this.title,
         description: description ?? this.description,
+        synced: synced,
       );
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson({bool online = false}) => {
         'k': key,
         't': title,
         'd': description,
+        's': !online ? synced : null,
       };
 }
