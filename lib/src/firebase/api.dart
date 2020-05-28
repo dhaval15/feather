5aad1cb9e7534f18bed90d8b53695a593755cb12
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-28 22:14:38 +0530

    Added Local Database api with Hive

--- lib/src/firebase/api.dart
+++ lib/src/firebase/api.dart
@@ -1,24 +1,85 @@
+import 'package:flutter/material.dart';
+
 import 'response.dart';
 import '../models/models.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hive/hive.dart';
 import 'refs.dart';
+import 'package:uuid/uuid.dart';
 
-mixin Api {
-  Future<Response> addCollection(Collection collection);
-  Future<Response> updateCollection(Collection collection);
-  Future<Response> addNote(Note note);
-  Future<Response> updateNote(Note note);
+mixin Api<T> {
+  T addCollection(Collection collection);
+  T updateCollection(Collection collection);
+  T addNote(Note note);
+  T updateNote(Note note);
 }
 
-// Default Implementation
+// Local Implementation
 
-class FirebaseApi with Api {
+class LocalApi with Api<dynamic> {
+  final Box collectionBox;
+  final Box noteBox;
+  final uuid = Uuid();
+
+  LocalApi({@required this.collectionBox, @required this.noteBox});
+
+  @override
+  Collection addCollection(Collection collection) {
+    final newCollection = collection.copyWith(key: uuid.v1());
+    collectionBox.put(newCollection.key, newCollection);
+    return newCollection;
+  }
+
+  @override
+  Note addNote(Note note) {
+    final newNote = note.copyWith(key: uuid.v1());
+    noteBox.put(newNote.key, newNote);
+    return newNote;
+  }
+
+  @override
+  Collection updateCollection(Collection collection) {
+    collectionBox.put(collection.key, collection);
+    return collection;
+  }
+
+  @override
+  Note updateNote(Note note) {
+    noteBox.put(note.key, note);
+    return note;
+  }
+}
+
+// Firebase Implementation
+
+class FirebaseApi with Api<Future<Response>> {
   final String userId;
 
   const FirebaseApi._(this.userId);
 
-  factory FirebaseApi.ofUser(FirebaseUser user) => FirebaseApi._(user.uid);
+  factory FirebaseApi.ofUser(FirebaseUser user) => FirebaseApi._(
+        user.uid,
+      );
+
+  Future<Response> syncOnline(LocalApi localApi) async {
+    localApi.collectionBox.values.forEach((value) async {
+      final collection = Collection.fromJson(value);
+      if (!collection.synced) {
+        final response = await updateCollection(collection);
+        if (response.isSuccessful)
+          localApi.updateCollection(collection.copyWith(synced: true));
+      }
+    });
+    localApi.noteBox.values.forEach((value) async {
+      final note = Note.fromJson(value);
+      if (!note.synced) {
+        final response = await updateNote(note);
+        if (response.isSuccessful)
+          localApi.addNote(note.copyWith(synced: true));
+      }
+    });
+  }
 
   @override
   Future<Response> addCollection(Collection collection) async {
@@ -29,7 +90,7 @@ class FirebaseApi with Api {
         .push();
     final newCollection = collection.copyWith(key: newReferenence.key);
     try {
-      await newReferenence.set(newCollection.toJson());
+      await newReferenence.set(newCollection.toJson(online: true));
       return Response.success(newCollection);
     } catch (e) {
       return Response.failure(e);
@@ -42,7 +103,7 @@ class FirebaseApi with Api {
         FirebaseDatabase.instance.reference().child(userId).child(NOTES).push();
     final newNote = note.copyWith(key: newReferenence.key);
     try {
-      await newReferenence.set(newNote.toJson());
+      await newReferenence.set(newNote.toJson(online: true));
       return Response.success(newNote);
     } catch (e) {
       return Response.failure(e);
@@ -57,7 +118,7 @@ class FirebaseApi with Api {
         .child(COLLECTIONS)
         .child(collection.key);
     try {
-      await oldReferenence.set(collection.toJson());
+      await oldReferenence.set(collection.toJson(online: true));
       return Response.success(collection);
     } catch (e) {
       return Response.failure(e);
@@ -72,7 +133,7 @@ class FirebaseApi with Api {
         .child(NOTES)
         .child(note.key);
     try {
-      await oldReferenence.set(note.toJson());
+      await oldReferenence.set(note.toJson(online: true));
       return Response.success(note);
     } catch (e) {
       return Response.failure(e);
