dce24a26b929f4989c66415b92295e91475c751f
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-05-28 22:12:02 +0530

    Added Syncing capabilities

--- lib/src/models/note.dart
+++ lib/src/models/note.dart
@@ -3,27 +3,44 @@ class Note {
   final String title;
   final String content;
   final String collectionId;
+  final bool synced;
 
-  Note({this.key, this.title, this.content, this.collectionId});
+  Note({
+    this.key,
+    this.title,
+    this.content,
+    this.collectionId,
+    this.synced = false,
+  });
 
   factory Note.fromJson(dynamic json) => Note(
         key: json['k'],
         title: json['t'],
         content: json['c'],
         collectionId: json['i'],
+        synced: json['s'],
       );
 
-  Note copyWith({String key, String title, String content}) => Note(
+  Note copyWith({
+    String key,
+    String title,
+    String content,
+    String collectionId,
+    bool synced,
+  }) =>
+      Note(
         key: key ?? this.key,
         title: title ?? this.title,
         content: content ?? this.content,
         collectionId: collectionId ?? this.collectionId,
+        synced: synced ?? this.synced,
       );
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson({bool online = false}) => {
         'k': key,
         't': title,
         'c': content,
         'i': collectionId,
+        's': !online ? synced : null,
       };
 }
